026c65a744b4b0c3ff2f675e829c825d6cbe833b
Jordi <jordi.broeken@gmail.com>
2014-10-31 11:31:29 +0100

    weer aanpassinge GUI

--- code/GUI.py
+++ code/GUI.py
@@ -4,33 +4,37 @@ from Tkinter import *
 import ask_questions_defs as ask
 #create the window
 filename = "begrippen_volgens_tqc.csv"
+question_mode = True
 mGui = Tk()
 ment = StringVar()
 
 v = IntVar()
 v.set(1)
+answer= StringVar()
+question_text =StringVar()
 res = ask.match_word_gui(filename,"random")
 answers = [
-    ["Python",1],
-    ["Perl",2],
-    ["Java",3],
-    ["C++",4],
+    ["Python",0],
+    ["Perl",1],
+    ["Java",2],
+    ["C++",3],
      ]
 
 for ii in range(0,4):
 	answers[ii][0] = res[1][ii]
 
-def ShowChoice():
-    print v.get()
+
+	
 
 #modify root window
 mGui.title("Simple GUI")
 #mGui.geometry("600x500")
 
 question_box = Label(mGui,
-      text = res[0],wraplength=500,
+      textvariable =question_text ,wraplength=500,
       justify = LEFT,
       padx = 20).pack(anchor=W)
+question_text.set(res[0])
 rb = []
 for txt, val in answers:
 	rb.append(Radiobutton (mGui,
@@ -41,8 +45,21 @@ for txt, val in answers:
 
 
 
-mButton = Button(mGui,text ='Submit', command=ShowChoice).pack()
+answer_box =Label(mGui,textvariable = answer,justify = LEFT, padx = 20).pack(anchor = E)
+def ShowChoice(question_mode):
+	if question_mode:
+		question_mode = False
+		if v.get()==res[2]:
+			print "bla"
+			answer.set("Goed")
+		else:
+			print "bla"
+			answer.set("Fout")
+	else:
+		question_mode = True
+		print "bla"
 
+mButton = Button(mGui,text ='Submit', command=ShowChoice(question_mode)).pack()
 #kick off the event loop
 if sys.platform.startswith('win32'):
     mGui.mainloop()
